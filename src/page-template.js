a0ef9ac0f8e36fe278e6a703ed61b4ec8512fd6e
rrich-kray <rrich.kray@gmail.com>
2022-02-26 15:04:42 -0500

    minimal addition

--- src/page-template.js
+++ src/page-template.js
@@ -1,23 +1,33 @@
-const generateTableOfContents = answers => {
-    let totalContent = ``;
-    Object.keys(answers).map(answer => totalContent += `\n\t[${answer}](#${answer})`)
-    return totalContent
-}
+const generateTableOfContents = (answers) => {
+  let totalContent = ``;
+  Object.keys(answers).map(
+    (answer) => (totalContent += `\n\t[${answer}](#${answer})`)
+  );
+  return totalContent;
+};
 
-const generateLicenseBadges = licenses => {
-    let totalContent = '';
-    licenses.map(license => {
-        if (license === 'MIT') totalContent += '\n\t[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
-        if (license === 'ISC') totalContent += '\n\t[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)'
-        if (license === 'Mozilla') totalContent += '\n\t[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
-    })
-    return totalContent;
-}
+const generateLicenseBadges = (licenses) => {
+  let totalContent = "";
+  licenses.map((license) => {
+    if (license === "MIT")
+      totalContent +=
+        "\n\t[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    if (license === "ISC")
+      totalContent +=
+        "\n\t[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+    if (license === "Mozilla")
+      totalContent +=
+        "\n\t[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+  });
+  return totalContent;
+};
 
-const generatePage = userInput => {
-    return `
+const generatePage = (userInput) => {
+  return `
     
     # ${userInput.title}
+
+    This repository contains the source code for the ${userInput.title} project.
 
     ## Description
 
@@ -55,7 +65,7 @@ const generatePage = userInput => {
 
     find my GitHub profile by following the link below:
 
-    ${userInput.github}`   
-}
+    ${userInput.github}`;
+};
 
 module.exports = generatePage;
